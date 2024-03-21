06b84d929ccd387a86b8f6fa405d0572353280bb
PedroBelfort <pedroamericocostabelfort@gmail.com>
2024-03-21 11:09:45 +0000

    RGB for the first method. Fake method response

---
+++ WordPuzzle/WordPuzzleSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPuzzle
+{
+    public class WordPuzzleSolver
+    {
+        public string ReplaceCharBySameIndex(string start, string end, int index)
+        {
+            return "CAME";
+        }
+    }
+}
